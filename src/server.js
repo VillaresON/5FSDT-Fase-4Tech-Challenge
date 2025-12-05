d9a455a60a1114de647bded299a5fe4d5e41cab6
Jonathas Villares <113390493+VillaresON@users.noreply.github.com>
2025-12-04 22:41:13 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -13,5 +13,9 @@ app.use("/posts", require("./routes/postRoutes"));
 app.use("/teachers", require("./routes/teacherRoutes"));
 app.use("/students", require("./routes/studentRoutes"));
 
+// Registrar rota de usuários
+app.use("/users", require("./routes/user"));
+
+// Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
